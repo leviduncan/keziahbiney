4f4dccc134c1f5aeeb2c6a1e05ff0927419f9c5f
darrin@darrinduncan.com <darrin@darrinduncan.com>
2022-10-15 17:30:02 -0400

    test

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -10,6 +10,7 @@ const About = () => {
                 <h2>About Keziah</h2>
                 <p>
                 Born to the father of Pentecostalism in the western region BISHOP REGINALD OFORI TWUMASI, Keziah was introduced to the HOLY SPIRIT and HIS workings at a very early age. As early as age 3 she and her sisters will sing under a unique anointing bringing crowds to tears and registering countless miracles. Now a resounding voice amongst Christian women groups in America and certain western and eastern African countries, Keziah is not only preaching, teaching, and pastoring, she is at the forefront of evangelism. Anointed to extend Divine strands of hope, healing, and restoration to the masses, Keziah is now proliferating the music scene with her unprecedented style and depth. A proud married woman and a mother of 4, Keziah still dedicates her time to all of her callings and giftings leaving none behind. A present unavoidable voice for our generation.
+                <span>Netlify...</span>
                 </p>
             </div>
     </div>
